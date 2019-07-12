72a5e575d2a3325fb77ab86eab61112fa0b8c72d
prernsin <prernsin@LNAR-5CG9204ZZH.corp.capgemini.com>
2019-07-12 17:15:32 -0400

    Testing with ip

--- training/src/main/java/ci/training/App.java
+++ training/src/main/java/ci/training/App.java
@@ -10,6 +10,6 @@ public class App{
 	
     public static void main( String[] args ){
     	
-        System.out.println( "Hello World testing!!!!!" );
+        System.out.println( "Hello World testing 123!!!!!" );
     }
 }
