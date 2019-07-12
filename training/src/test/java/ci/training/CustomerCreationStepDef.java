1da93127d7dc3bd61b7f3706dec6aed3f34a77b4
Singh <prerna.a.singh@capgemini.com>
2019-07-12 15:34:54 -0400

    testing

--- training/src/test/java/ci/training/CustomerCreationStepDef.java
+++ training/src/test/java/ci/training/CustomerCreationStepDef.java
@@ -16,17 +16,16 @@ public class CustomerCreationStepDef {
 	WalletServiceImpl walletService;
 	Customer res;
 	@Given("^I have customerDetail$")
-	public void i_have_customerDetail() throws Throwable { 
-		System.out.println("Checking");
+	public void i_have_customerDetail() { 
 		walletService = new WalletServiceImpl(repo);
 	}
 	@When("^I have \"([^\"]*)\" \"([^\"]*)\" and (\\d+)$")
-	public void i_have_and(String name, String phoneNumber, BigDecimal balance) throws Throwable {
+	public void i_have_and(String name, String phoneNumber, BigDecimal balance){
 		res = walletService.createWallet(name, phoneNumber, balance);
 	}
 
 	@Then("^I should get \"([^\"]*)\" \"([^\"]*)\" and (\\d+)$")
-	public void i_should_get_and(String name, String phoneNumber, BigDecimal balance) throws Throwable {
+	public void i_should_get_and(String name, String phoneNumber, BigDecimal balance){
 		assertThat(res.getName()).isEqualTo(name);
 		assertThat(res.getPhoneNumber()).isEqualTo(phoneNumber);
 		assertThat(res.getWallet().getBalance()).isEqualTo(balance);
